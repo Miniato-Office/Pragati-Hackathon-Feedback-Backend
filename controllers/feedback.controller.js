c14e2c52caac7d3c419c6ae9bd5058b99806b977
Jatin <96469998+JatinSharma32@users.noreply.github.com>
2023-09-23 20:02:44 +0530

    dev: Controllers done Enhancement & Validation pending (#2)

--- controllers/feedback.controller.js
+++ controllers/feedback.controller.js
@@ -1,7 +1,18 @@
-import { errorHandler } from "../utils/errorHandler";
+import { errorHandler } from "../utils/errorHandler.js";
+import {
+    findFeedbackInDB,
+    storeFeedbackInDB,
+} from "../models/feedback.model.js";
 
-export const storingFeedback = (req, res) => {
+export const storingFeedback = async (req, res) => {
     try {
+        const { feedbackContent, userUniqueId } = req.body;
+        const storeFeedbackStatus = await storeFeedbackInDB({
+            feedbackContent,
+            userUniqueId,
+        });
+        console.log("Store Feedback Status: ", storeFeedbackStatus);
+        res.status(200).json({});
     } catch (error) {
         errorHandler(error, res);
     }
